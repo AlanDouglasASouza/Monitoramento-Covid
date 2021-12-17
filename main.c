70fe4525d98d972a750d66a5edd467aebb6e1ccd
Alan Douglas A de Souza <alan.douglas.ads@gmail.com>
2021-12-16 23:16:44 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <conio.h>
+
+//Struct de login
+typedef struct{
+    char login[30];
+    char senha[30];
+} Usuario;
+Usuario usuario[1];
+
+void login();
+void cabecalho();
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    cabecalho();
+    login();
+    return 0;
+}
+
+void cabecalho(){
+    printf("\n**********************************************************************\n");
+    printf("\n----------------------- MONITORAMENTO COVID-19 -----------------------\n");
+    printf("\n**********************************************************************\n");
+}
+
+void login(){
+    char login[30];
+    char senha[30];
+
+    strcpy(usuario[0].login, "admin"); // Definindo o login "admin" na struct
+    strcpy(usuario[0].senha, "admin"); // Definindo a senha "admin"  na struct
+
+    printf("\nLogin: ");
+    scanf("%s", login); // armazenando os dados inseridos pelo usuário para o vetor login que está dentro da função main
+
+    printf("\nSenha: ");
+    scanf("%s", senha); // armazenando os dados inseridos pelo usuário para o vetor senha que está dentro da função main
+
+    if ((strcmp(login,usuario[0].login)==0) && (strcmp(senha,usuario[0].senha)==0)){ // A função strcmp é responsável por comparar string com string
+        printf("Usuário logado!");// se os vetores de dentro da struct tiver os mesmos dados do vetor interno da função main, usuário será logado.
+        system("cls");
+    }else{
+        printf("Login e/ou senha incorretos");
+    }
+}
